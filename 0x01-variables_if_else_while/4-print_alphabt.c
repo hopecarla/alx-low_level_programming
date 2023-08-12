ff77fca39cf220a191427d15d5a87c5e3a9392aa
hopecarla <hopememba.carla@gmail.com>
2023-08-12 03:53:37 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0
+ */
+int main(void)
+{
+	char i;
+	char j;
+
+for (i = 'a'; i <= 'z'; i++)
+putchar(i);
+for (j = 'A'; j <= 'Z'; j++)
+putchar(j);
+	putchar('\n');
+}
