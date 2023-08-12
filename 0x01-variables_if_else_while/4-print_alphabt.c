dd1f10d2be829ac30b8c813811d8186102164697
hopecarla <hopememba.carla@gmail.com>
2023-08-12 04:06:24 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,11 +6,13 @@
 int main(void)
 {
 	char i;
-	char j;
 
-for (i = 'a'; i <= 'z'; i++)
-putchar(i);
-for (j = 'A'; j <= 'Z'; j++)
-putchar(j);
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	{
+		if (i != 'e' && i != 'q')
+			putchar(i);
+	}
 	putchar('\n');
+	return (0);
 }
