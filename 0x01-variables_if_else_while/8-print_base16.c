4279f2b2c08f556466673310d1e998ddce31236a
hopecarla <hopememba.carla@gmail.com>
2023-08-12 05:08:33 -0700

    8-print

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i <= 58; i++)
+		putchar('i');
+	for (j = 'a'; j <= 'z'; j++)
+		putchar('j');
+	return (0);
+}
