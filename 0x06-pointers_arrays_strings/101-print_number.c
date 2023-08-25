cc432cb3a615be3784178673d576abecc413bd33
hopecarla <hopememba.carla@gmail.com>
2023-08-25 08:02:39 -0700

    101

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_number - that prints an integer
+ * @n: number to be checked
+ * Return: 0
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+	n = -n;
+	}
+
+	if (n - n  == 0)
+	{
+		_putchar('0');
+	}
+
+}
