479d9537d375ab64e95c018ee27819532c49f01a
hopecarla <hopememba.carla@gmail.com>
2023-08-30 02:00:06 -0700

    2-strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *  _strlen_recursion - function that returns the length of a string
+ *  @s: string
+ *  Return: 0
+ */
+int _strlen_recursion(char *s)
+{
+	int length = 0;
+
+	if (*s)
+	{
+		length++;
+		length += _strlen_recursion(s + 1);
+	}
+	return (length);
+}
