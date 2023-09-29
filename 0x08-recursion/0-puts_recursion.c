1550bd18b2e7dd8d1ddf6803a7e5693773872530
hopecarla <hopememba.carla@gmail.com>
2023-09-29 12:16:17 -0700

    0-puts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,13 +3,14 @@
 /**
  * _puts_recursion -  prints a string, followed by a new line
  * @s: string to be printed
- * Return: the string
+ * Return: 0
  */
 void _puts_recursion(char *s)
 {
-	int length = strlen(s);
-
-	if (s != '/0')
-		_puts_recursion(s);
-	return (s);
+	if (*s != '\0')
+	{
+		_putchar('\n');
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
