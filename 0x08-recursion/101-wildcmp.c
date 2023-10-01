a32142c6621a5412660c843f9448665bdd5a63a7
hopecarla <hopememba.carla@gmail.com>
2023-10-01 04:51:17 -0700

    101

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * wildcmp - ompares two strings
+ * @s1: string one
+ * @s2: string two
+ * Return: 1 if they equal 0 if they are not
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	if (*s1 == *s2 || *s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2 + 1) || wildcmp(s1, s2 + 1));
+	}
+	return (0);
+}
