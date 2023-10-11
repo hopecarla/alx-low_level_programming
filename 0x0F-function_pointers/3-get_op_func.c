df596b4fef92bc32f13695f4acc24e4ba6a26f3b
hopecarla <hopememba.carla@gmail.com>
2023-10-11 02:25:27 -0700

    functions

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -9,13 +9,13 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-	p_t ops[] = {
+	op_t ops[] = {
         {"+", op_add},
         {"-", op_sub},
         {"*", op_mul},
         {"/", op_div},
         {"%", op_mod},
-        {NULL, NULL}
+        {NULL, NULL},
     };
     int i = 0;
 
