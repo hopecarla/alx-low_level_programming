9feb313f542d3e98ddd933b3b2fa4073987656be
hopecarla <hopememba.carla@gmail.com>
2023-10-22 07:43:57 -0700

    100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,13 @@
-#include <stdio.h>
+nclude <stdio.h>
+
+void first(void) __attribute__ ((constructor));
+
 /**
- * print - prints a sentence before the main function is executed
+ * first - prints a sentence before the main
+ * function is executed
  */
-void print(void)
+void first(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+ printf("You're beat! and yet, you must allow,\n");
+ printf("I bore my house upon my back!\n");
 }
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,41 +1,37 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lists.h"
-
 /**
- * add_node_end - adds a new node at the end of a linked list
+ * add_node_end - adds a new node at the end of list_t list
  * @head: double pointer to the list_t list
  * @str: string to put in the new node
- *
  * Return: address of the new element, or NULL if it failed
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
- list_t *new;
- list_t *temp = *head;
- unsigned int len = 0;
+list_t *new_node;
+list_t *count = *head;
+unsigned int len = 0;
 
- while (str[len])
- len++;
+while (str[len])
+len++;
 
- new = malloc(sizeof(list_t));
- if (!new)
- return (NULL);
+new_node = malloc(sizeof(list_t));
+if (!new_node)
+return (NULL);
 
- new->str = strdup(str);
- new->len = len;
- new->next = NULL;
+new_node->str = strdup(str);
+new_node->len = len;
+new_node->next = NULL;
 
- if (*head == NULL)
- {
- *head = new;
- return (new);
- }
+if (*head == NULL)
+{
+*head = new_node;
+return (new_node);
+}
 
- while (temp->next)
- temp = temp->next;
-
- temp->next = new;
-
- return (new);
+while (count->next)
+count = count->next;
+count->next = new_node;
+return (new_node);
 }
